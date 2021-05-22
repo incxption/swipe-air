c53afee848fa8f6b7cd38982e4b700e24106a127
moritz <theincxption@gmail.com>
2021-05-22 23:43:44 +0200

    feat(ionic): cleanup Angular & create welcome page with tutorial slides

--- ionic/src/app/app.module.ts
+++ ionic/src/app/app.module.ts
@@ -1,32 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { StatusBar, StatusBarStyle } from '@capacitor/status-bar';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { StatusBar, StatusBarStyle } from "@capacitor/status-bar";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot({
-      rippleEffect: true,
-      mode: 'ios',
-    }),
-    AppRoutingModule,
-  ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot({
+            rippleEffect: true,
+            mode: "ios",
+        }),
+        AppRoutingModule,
+    ],
+    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    bootstrap: [AppComponent],
 })
 export class AppModule {
-  constructor() {
-    (async () => {
-      await StatusBar.setOverlaysWebView({ overlay: false });
-      await StatusBar.setStyle({ style: StatusBarStyle.Dark });
-    })();
-  }
+    constructor() {
+        (async () => {
+            await StatusBar.setOverlaysWebView({ overlay: true });
+            await StatusBar.setStyle({ style: StatusBarStyle.Light });
+        })();
+    }
 }
