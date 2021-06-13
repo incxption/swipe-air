2b5eb5af28e261c7c302fb556874a17227293d36
moritz <theincxption@gmail.com>
2021-06-13 14:23:57 +0200

    Delete legacy Android app
    Signed-off-by: moritz <theincxption@gmail.com>

--- android/app/src/main/java/net/theincxption/connectify/client/Packet.kt
+++
@@ -1,3 +0,0 @@
-package net.theincxption.connectify.client
-
-open class Packet(val event: String, vararg val arguments: Any?)
--- android/app/src/main/java/net/theincxption/connectify/client/YouTubeVideoPacket.kt
+++
@@ -1,4 +0,0 @@
-package net.theincxption.connectify.client
-
-data class YouTubeVideoPacket(val title: String, val channel: String, val timestamp: String) :
-    Packet("share youtube video", title, channel, timestamp)
